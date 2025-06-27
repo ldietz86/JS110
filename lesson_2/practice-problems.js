bc713dca6c0e63bb9411e0ee9f1ac58a04c5fae5
Lakpa Dietz <ldietz086@gmail.com>
2025-06-27 10:36:48 -0700

    Practice problems 5-10

--- lesson_2/practice-problems.js
+++ lesson_2/practice-problems.js
@@ -29,27 +29,29 @@ let books = [
   { title: "The Book of Kells", author: "Multiple Authors", published: "800" },
 ];
 
-let orderByDate = books.sort((a,b) => Number(a.published) - Number(b.published));
+let orderByDate = books.sort(
+  (a, b) => Number(a.published) - Number(b.published)
+);
 
 //3. For each of these collections, access the letter g.
 
 let arr1 = ["a", "b", ["c", ["d", "e", "f", "g"]]];
-arr1[2][1][3]
+arr1[2][1][3];
 
 let arr2 = [
   { first: ["a", "b", "c"], second: ["d", "e", "f"] },
   { third: ["g", "h", "i"] },
 ];
-arr2[1].third[0]
+arr2[1].third[0];
 
 let arr3 = [["abc"], ["def"], { third: ["ghi"] }];
-arr3[2].third[0][0]
+arr3[2].third[0][0];
 
 let obj1 = { a: ["d", "e"], b: ["f", "g"], c: ["h", "i"] };
-obj1.b[1]
+obj1.b[1];
 
 let obj2 = { first: { d: 3 }, second: { e: 2, f: 1 }, third: { g: 0 } };
-Object.keys(obj2.third).toString()
+Object.keys(obj2.third).toString();
 //Object.keys(obj2.third)[0];
 
 //4. For each of these collection objects, change the value 3 to 4.
@@ -78,3 +80,130 @@ let munsters = {
   Marilyn: { age: 23, gender: "female" },
 };
 
+let totalMaleAge = 0;
+
+for (let member in munsters) {
+  if (munsters[member].gender === "male") {
+    totalMaleAge += munsters[member].age;
+  }
+}
+
+console.log(totalMaleAge);
+
+//Using Object.values
+let memberDetails = Object.values(munsters); // [{age: 32, gender: 'male'}, {age: 30, gender: 'female'}, ...]
+let totalMaleAge = 0;
+
+memberDetails.forEach((member) => {
+  if (member.gender === "male") {
+    totalMaleAge += member.age;
+  }
+});
+
+console.log(totalMaleAge);
+
+//6. print the name, age, and gender of each family member in the munsters object.
+let munsters = {
+  herman: { age: 32, gender: "male" },
+  lily: { age: 30, gender: "female" },
+  grandpa: { age: 402, gender: "male" },
+  eddie: { age: 10, gender: "male" },
+  marilyn: { age: 23, gender: "female" },
+};
+
+for (let member in munsters) {
+  console.log(
+    `${member} is a ${munsters[member]?.age}-year-old ${munsters[member]?.gender}`
+  );
+}
+
+//Alternative solution using destructuring
+for (let member in munsters) {
+  const { age, gender } = munsters[member];
+  console.log(`${member} is a ${age}-year-old ${gender}`);
+}
+
+//LS solution using Object.entries
+Object.entries(munsters).forEach((entry) => {
+  let name = entry[0];
+  let age = entry[1]["age"];
+  let gender = entry[1].gender;
+
+  console.log(`${name} is a ${age}-year-old ${gender}.`);
+});
+
+//7. What will the final values of a and b be?
+let a = 2;
+let b = [5, 8];
+let arr = [a, b];
+
+arr[0] += 2;
+arr[1][0] -= a;
+
+//The final value of a is 2 and the final value of b is [3, 8]
+
+//8. Using the forEach method, write some code to output all vowels from the strings in the arrays.
+let obj = {
+  first: ["the", "quick"],
+  second: ["brown", "fox"],
+  third: ["jumped"],
+  fourth: ["over", "the", "lazy", "dog"],
+};
+
+let outerArr = Object.values(obj);
+
+outerArr.forEach((innerArr) => {
+  innerArr.forEach((word) => {
+    let chars = word.split("");
+    chars.forEach((char) => {
+      if ("aeiou".includes(char)) {
+        console.log(char);
+      }
+    });
+  });
+});
+
+//9. Return a new array with the same structure, but with the values in each subarray ordered -- alphabetically or numerically as appropriate -- in ascending order.
+let arr = [
+  ["b", "c", "a"],
+  [2, 11, -3],
+  ["blue", "black", "green"],
+];
+
+let sortedArr = arr.map((nestedArr) => {
+  if (typeof nestedArr[0] === "number") {
+    return nestedArr.slice().sort((a, b) => Number(a) - Number(b));
+  } else {
+    return nestedArr.slice().sort(); //use the sort method to make a shallow copy of the subarrays
+  }
+});
+
+console.log(sortedArr); // => [ [ 'a', 'b', 'c' ], [ -3, 2, 11 ], [ 'black', 'blue', 'green' ] ]
+
+//10. Sort the elements in descending order.
+let arr = [
+  ["b", "c", "a"],
+  [2, 11, -3],
+  ["blue", "black", "green"],
+];
+
+let sortedArr = arr.map((nestedArr) => {
+  if (typeof nestedArr[0] === "number") {
+    return nestedArr.slice().sort((a, b) => Number(b) - Number(a));
+  } else {
+    return nestedArr.slice().sort((a, b) => b.localeCompare(a));
+  }
+});
+
+console.log(sortedArr);
+
+//LS solution
+arr.map(subArray => {
+  return subArray.slice().sort((a, b) => {
+    if (a > b) return -1;
+    if (b > a) return 1;
+    return 0;
+  });
+});
+
+// => [ [ 'c', 'b', 'a' ], [ 11, 2, -3 ], [ 'green', 'blue', 'black' ] ]
