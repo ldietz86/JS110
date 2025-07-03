19c675d12ff82f684106a22a74c19e04fb190dd1
Lakpa Dietz <ldietz086@gmail.com>
2025-07-03 11:43:05 -0700

    Tic tac toe step 1 & 2

---
+++ lesson_3/tictactoe.js
@@ -0,0 +1,67 @@
+let readline = require("readline-sync");
+
+const INITIAL_MARKER = " ";
+const HUMAN_MARKER = "X";
+const COMPUTER_MARKER = "O";
+
+//Step 1 - Set up and display the board
+function displayBoard(board) {
+  console.log("");
+  console.log("     |     |");
+  console.log(`  ${board["1"]}  |  ${board["2"]}  |  ${board["3"]}`);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${board["4"]}  |  ${board["5"]}  |  ${board["6"]}`);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${board["7"]}  |  ${board["8"]}  |  ${board["9"]}`);
+  console.log("     |     |");
+  console.log("");
+}
+
+function initializeBoard() {
+  let board = {};
+
+  for (let square = 1; square <= 9; square++) {
+    board[String(square)] = INITIAL_MARKER;
+  }
+
+  return board;
+}
+
+//Step 2 - Player and Computer Turn
+function emptySquares(board) {
+  return Object.keys(board).filter((key) => board[key] === INITIAL_MARKER);
+}
+
+function playerChoosesSquare(board) {
+  let square;
+
+  while (true) {
+    square = readline
+      .question(`Choose a square (${emptySquares(board).join(", ")}): `)
+      .trim();
+    if (emptySquares(board).includes(square)) break;
+
+    console.log("Invalid entry.");
+  }
+
+  board[square] = HUMAN_MARKER;
+}
+
+function computerChoosesSquare(board) {
+  let randomIndex = Math.floor(Math.random() * emptySquares(board).length);
+
+  let square = emptySquares(board)[randomIndex];
+  board[square] = COMPUTER_MARKER;
+}
+
+let board = initializeBoard();
+displayBoard(board);
+
+playerChoosesSquare(board);
+computerChoosesSquare(board);
+
+displayBoard(board);
